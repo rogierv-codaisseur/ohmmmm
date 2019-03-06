5cdac1b233af6469eba6f834138456a0844fe9a2
LeoniePeters <leoniepeters@gmail.com>
2019-03-06 17:40:20 +0100

    add animation flower

--- client/src/game/index.js
+++ client/src/game/index.js
@@ -25,7 +25,9 @@ export default () => {
   };
 
   var player;
-  var ohms;
+  var gohms;
+  var pohms;
+  var oohms
   var score = 0;
   var scoreText;
   var timedEvent;
@@ -39,33 +41,75 @@ export default () => {
     this.load.image('gohm', 'assets/DotGreen.png');
     this.load.image('pohm', 'assets/DotPurple.png');
     this.load.image('oohm', 'assets/DotOrange.png');
-    this.load.spritesheet('player', 'assets/Player.png', {
-      frameWidth: 150,
-      frameHeight: 150
-    });
+    this.load.image('player150', 'assets/Player150.png');
+    this.load.image('player140', 'assets/Player140.png');
+    this.load.image('player130', 'assets/Player130.png');
+    this.load.image('player120', 'assets/Player120.png');
   }
 
   function create() {
+
+    // add background image
     this.add.image(200, 350, 'stage');
 
+    // add timer text to canvas
     timeText = this.add.text(300, 0, '', {
       fontSize: '16px',
       fill: '#000'
     });
 
+    // create timer - set time in ms
     timedEvent = this.time.delayedCall(5000, onEvent, [], this);
 
-    player = this.physics.add.sprite(75, 625, 'player').setInteractive();
+    this.anims.create({
+      key: 'breath',
+      frames: [
+        { key: 'player120' },
+        { key: 'player130' },
+        { key: 'player140' },
+        { key: 'player150', duration: 2000 },
+        { key: 'player140' },
+        { key: 'player130' },
+        { key: 'player120', duration: 2000 },
+      ],
+      frameRate: 8,
+      repeat: -1
+    });
 
-    this.input.on('pointermove', function(pointer) {
+    // create player
+    player = this.physics.add.sprite(75, 625, 'player150').setInteractive().play('breath');
+
+    // define player movements
+    this.input.on('pointermove', function (pointer) {
+
       player.x = pointer.x;
       player.y = pointer.y;
     });
 
-    //  Create 10 sprites (they all start life at 0x0)
-    ohms = this.physics.add.group({
+    //  Create 5 GREEN ohms
+    gohms = this.physics.add.group({
       key: 'gohm',
-      frameQuantity: 10,
+      frameQuantity: 5,
+      // this scale does not work
+      scale: {
+        randFloat: [0.5, 1.5]
+      }
+    });
+
+    //  Create 5 PURPLE ohms
+    pohms = this.physics.add.group({
+      key: 'pohm',
+      frameQuantity: 5,
+      // this scale does not work
+      scale: {
+        randFloat: [0.5, 1.5]
+      }
+    });
+
+    //  Create 5 ORANGE ohms
+    oohms = this.physics.add.group({
+      key: 'oohm',
+      frameQuantity: 5,
       // this scale does not work
       scale: {
         randFloat: [0.5, 1.5]
@@ -74,49 +118,77 @@ export default () => {
 
     var rect = new Phaser.Geom.Rectangle(0, 0, 400, 700);
 
-    //  Randomly position the sprites within the rectangle
-    Phaser.Actions.RandomRectangle(ohms.getChildren(), rect);
+    //  Randomly position the ohms within the rectangle
+    Phaser.Actions.RandomRectangle(gohms.getChildren(), rect);
+    Phaser.Actions.RandomRectangle(pohms.getChildren(), rect);
+    Phaser.Actions.RandomRectangle(oohms.getChildren(), rect);
 
+    // add score text
     scoreText = this.add.text(0, 0, 'score: 0', {
       fontSize: '16px',
       fill: '#000'
     });
 
-    this.physics.add.overlap(player, ohms, collectOhms, null, this);
+    // set up overlap to trigger the scoring
+    this.physics.add.overlap(player, gohms, collectGohms, null, this);
+    this.physics.add.overlap(player, pohms, collectPohms, null, this);
+    this.physics.add.overlap(player, oohms, collectOohms, null, this);
 
-    function collectOhms(player, ohms) {
-      ohms.disableBody(true, true);
+    // collect GREEN ohms
+    function collectGohms(player, gohms) {
+
+      gohms.disableBody(true, true);
 
       score += 10;
       scoreText.setText('Score: ' + score);
 
       // if (ohms.countActive(true) === 0) {
-
       //   return Phaser.Actions.RandomRectangle(ohms.getChildren(), rect);
+      // }
+    }
+    
+    // collect PURPLE ohms
+    function collectPohms(player, pohms) {
 
+      pohms.disableBody(true, true);
+
+      score += 10;
+      scoreText.setText('Score: ' + score);
+
+      // if (ohms.countActive(true) === 0) {
+      //   return Phaser.Actions.RandomRectangle(ohms.getChildren(), rect);
+      // }
+    }
+
+    // collect ORANGE ohms
+    function collectOohms(player, oohms) {
+
+      oohms.disableBody(true, true);
+
+      score += 10;
+      scoreText.setText('Score: ' + score);
+
+      // if (ohms.countActive(true) === 0) {
+      //   return Phaser.Actions.RandomRectangle(ohms.getChildren(), rect);
       // }
     }
   }
 
   function update() {
-    if (gameOver) {
-      return;
-    }
 
-    timeText.setText(
-      'Time: ' +
-        timedEvent
-          .getElapsedSeconds()
-          .toString()
-          .substr(0, 3)
-    );
+    // displays the current timer in seconds
+    timeText.setText('Time: ' + timedEvent.getElapsedSeconds().toString().substr(0, 3));
   }
 
   function onEvent() {
+
+    // pauses the game when timer runs out
     this.physics.pause();
 
-    gameOver = true;
+    // set gameOver to false, even though this does nothing yet
+    gameOver = true
 
+    // pop up text when timer runs out
     let gameOverText = this.add.text(30, 270, 'GOOD JOB!', {
       fontSize: '64px',
       fill: '#000'
