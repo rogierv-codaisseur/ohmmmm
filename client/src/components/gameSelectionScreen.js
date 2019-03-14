bf076c6f384a62520e808fb727936cec66e8af99
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-03-14 12:13:47 +0100

    game selection styling revised

--- client/src/components/gameSelectionScreen.js
+++ client/src/components/gameSelectionScreen.js
@@ -12,6 +12,7 @@ const GameSelectionScreen = ({ currentUser, logoff, setGame }) => {
 
       <div className="Two">
         <Link to="/pre-game" onClick={() => setGame(120, 1)}>
+          <img className="red" src="../assets/red.png" alt="red" />
           <span className="Time">2</span>
           <br />
           <span className="Minutes">minutes</span>
@@ -19,6 +20,7 @@ const GameSelectionScreen = ({ currentUser, logoff, setGame }) => {
       </div>
       <div className="Five">
         <Link to="/pre-game" onClick={() => setGame(300, 2)}>
+          <img className="orange" src="../assets/orange.png" alt="orange" />
           <span className="Time">5</span>
           <br />
           <span className="Minutes">minutes</span>
@@ -26,12 +28,13 @@ const GameSelectionScreen = ({ currentUser, logoff, setGame }) => {
       </div>
       <div className="Ten">
         <Link to="/pre-game" onClick={() => setGame(600, 3)}>
+          <img className="green" src="../assets/green.png" alt="green" />
           <span className="Time">10</span>
           <br />
           <span className="Minutes">minutes</span>
         </Link>
       </div>
-      <img className="exit" src="assets/exit_white.svg" onClick={logoff} alt="exit" />
+      {/* <img className="exit" src="assets/exit_white.svg" onClick={logoff} alt="exit" /> */}
     </div>
   );
 };
